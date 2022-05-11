24dc605bbe8f4bd2a83decd2dff2b76e125a8e82
Richard Clarke <freshnewrich@gmail.com>
2022-05-10 22:18:52 -0700

    button 5

--- src/components/example-5-css-modules/Button.jsx
+++ src/components/example-5-css-modules/Button.jsx
@@ -1,5 +1,19 @@
+//css modules
+
+//import this module.css file you create
+import styles from  "./button.module.css"
+
 export default function Button({ text }) {
     return (
-      <button>{ text }</button>
+      <button className={styles.btn5}>{ text }</button>
     );
   }
+
+// Pros
+// This scopes our css to this component file only
+// css file can be reused for multiple components but will not affect 
+//similar classes in other component css files
+
+// Cons
+// can be weird with typescript
+// syntax to do multiple class names can be weird - must use template literals
