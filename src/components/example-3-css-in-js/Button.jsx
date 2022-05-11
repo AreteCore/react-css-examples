784a79ad3470e158d4f3c8a18ce23d875021f51a
Richard Clarke <freshnewrich@gmail.com>
2022-05-10 20:46:00 -0700

    button 3

--- src/components/example-3-css-in-js/Button.jsx
+++ src/components/example-3-css-in-js/Button.jsx
@@ -1,5 +1,42 @@
+//css in js!
+
+//this is what MS Teams use to build their app
+//they built a component library for themselves, 
+//they use css in js to style the components in the app
+
+//to start using this this we run 
+//> npm install react-jss
+
+//then import
+import { createUseStyles } from "react-jss"
+
+//use their method to add a bunch of style objects
+//to one variable youre going to pass in
+const styles = createUseStyles({
+  btn: {
+    backgroundColor: "#da2",
+    borderRadius: "8px",
+    color: "white",
+    fontSize: "24pt",
+    padding: "15px 50px",
+    width: "300px",
+  },
+  btnColor: {
+    backgroundColor: "brown"
+  }
+})
+
+
 export default function Button({ text }) {
+
+//"styles" is a function
+//you have to declare a variable that runs styles() to expand them
+const classes = styles()
+
     return (
-      <button>{ text }</button>
+      <button className={ classes.btn }>{ text }</button>
     );
   }
+
+  // pro
+  //quick server side rendering
