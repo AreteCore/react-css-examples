261be4969973f50150cf52fbed47865515a94cab
Richard Clarke <freshnewrich@gmail.com>
2022-05-10 20:24:58 -0700

    button 2

--- src/components/example-2-inline-css/Button.jsx
+++ src/components/example-2-inline-css/Button.jsx
@@ -1,5 +1,32 @@
+//inline css styling in jsx
+
 export default function Button({ text }) {
+
+  //here we are
+  const buttonStyle = {
+    //define styles as key:value pairs
+    //cant use kebob case, use camelCase
+    //wrap the values in string quotes
+    backgroundColor: "#d45",
+    borderRadius: "8px",
+    color: "white",
+    fontSize: "24pt",
+    padding: "15px 50px",
+    width: "300px",
+  }
+
     return (
-      <button>{ text }</button>
+      <button style={buttonStyle}>{ text }</button>
     );
   }
+
+  // pros
+  //no extra file
+  //no class clashing
+  //you can use this with props/state and you can alter styles on the fly
+  //good for server side rendering
+
+  
+  // cons
+  //clutter?
+  //no real pseudoselectors, you have to use state instead
