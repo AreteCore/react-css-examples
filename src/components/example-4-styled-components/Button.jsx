8d8ec3e4cb1904c7643ca8981a8f7d6c26ec8604
Richard Clarke <freshnewrich@gmail.com>
2022-05-10 21:32:22 -0700

    button 4

--- src/components/example-4-styled-components/Button.jsx
+++ src/components/example-4-styled-components/Button.jsx
@@ -1,5 +1,24 @@
+//styled components
+//> npm install styled-components
+
+//import it
+import styled from "styled-components"
+
+// declare a variable to whatever you need
+//this .button is what you got down there in the return
+const MyNewStyledButton = styled.button`
+        background-color: #81a;
+        border-radius: 8px;
+        color: white;
+        font-size: 24pt;
+        width: 300px;
+`
+//this string is "tagged template literals" "template string" "string interpolation" etc
+//you can use regular css
+
+
 export default function Button({ text }) {
     return (
-      <button>{ text }</button>
+      <MyNewStyledButton>{ text }</MyNewStyledButton>
     );
   }
