caab67ef6db5943c03741939a2ed65ab9cfd52d5
Richard Clarke <freshnewrich@gmail.com>
2022-05-10 22:46:57 -0700

    buttopn 6

--- src/components/example-6-sass/Button.jsx
+++ src/components/example-6-sass/Button.jsx
@@ -7,3 +7,9 @@ import "./button.sass"
 export default function Button({ text }) {
   return <button className="sassy-button">{ text }</button>;
 }
+
+// pros
+//cool syntax 
+
+// cons
+//gotta install sass
