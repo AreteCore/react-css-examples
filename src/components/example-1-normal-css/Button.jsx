24a01bdf717a842af8640ab7cbc79f40213144f8
Richard Clarke <freshnewrich@gmail.com>
2022-05-10 19:20:46 -0700

    button 1

--- src/components/example-1-normal-css/Button.jsx
+++ src/components/example-1-normal-css/Button.jsx
@@ -1,5 +1,19 @@
+// import style sheet, easiest way
+import "./Button.css"
+
 export default function Button({ text }) {
     return (
-      <button>{ text }</button>
+      <button className="btn">{ text }</button>
     );
   }
+
+// pros of this link method
+//contained to one file
+//easy to find the file
+
+
+// cons
+//can conflict with other classes/duplicate classes and cause conflicts
+//this is not a great way to do things
+//too many files, bloats
+
